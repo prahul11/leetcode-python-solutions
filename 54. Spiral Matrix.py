71d2dc5db7c716925c10f7628fff5a2479517f01
Rahul Pandey <rhlpnd89@gmail.com>
2025-12-09 10:00:00 +0530

    Add 54. Spiral Matrix.py

---
+++ 54. Spiral Matrix.py
@@ -0,0 +1,54 @@
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+
+# class Solution:
+#     def spiralOrder(self, matrix) :
+#         res = []
+#         left, right = 0, len(matrix[0])
+#         top, bottom = 0, len(matrix)
+        
+#         while left < right and top < bottom:
+#             for i in range(left, right):
+#                 res.append(matrix[top][i])
+#             top += 1
+#             for i in range(top, bottom):
+#                 res.append(matrix[i][right -1])
+#             right -=1
+#             if not (left < right and top < bottom):
+#                 break
+#             for i in range(right-1, left-1, -1):
+#                 res.append(matrix[bottom-1][i])
+#             bottom -= 1
+#             for i in range(bottom -1, top -1 , -1):
+#                 res.append(matrix[i][left])
+#             left += 1
+#         return res
+
+
+
+class Solution:
+    def spiralOrder(self, matrix) :
+        res = []
+        left, right = 0, len(matrix[0])
+        top = 0
+        bottom = len(matrix)
+
+        while left < right and top < bottom :
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1
+            for i in range(top, bottom):
+                res.append(matrix[i][right -1])
+            right -= 1
+            if not(top < bottom and left < right):
+                break
+            for i in range(right-1, left -1 , -1):
+                res.append(matrix[bottom-1][i])
+            bottom -= 1
+            for i in range(bottom-1, top-1, -1):
+                res.append(matrix[i][left])
+            left +=1
+        return res
+
+
+t = Solution()
+print(t.spiralOrder(matrix))
